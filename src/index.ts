ac7b93a3be6cbc9f9abbf2999ec88bb81972ca06
Peerawit Pharkdeepinyo <peerawitp@protonmail.com>
2024-07-13 14:19:07 +0700

    feat: add prisma schema, cors and swagger

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,18 @@
 import { Elysia } from "elysia";
+import { cors } from "@elysiajs/cors";
+import { swagger } from "@elysiajs/swagger";
 
-const app = new Elysia().get("/", () => "Hello Elysia").listen(3000);
+const app = new Elysia()
+  .use(cors())
+  .use(swagger())
+  .get("/", () => "Hello Elysia")
+  .ws("/chatbot", {
+    message(ws, message) {
+      ws.send(message);
+    },
+  })
+  .listen(3000);
 
 console.log(
-  `🦊 Elysia is running at ${app.server?.hostname}:${app.server?.port}`
+  `🦊 Elysia is running at ${app.server?.hostname}:${app.server?.port}`,
 );
