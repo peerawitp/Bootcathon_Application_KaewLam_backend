723901fdc40e161fa5f958ef6781744bf48a2911
Peerawit Pharkdeepinyo <peerawitp@protonmail.com>
2024-07-13 15:56:13 +0700

    feat: add mobilOneCenter seeder

---
+++ prisma/seeders/center.js
@@ -0,0 +1,44 @@
+const { PrismaClient } = require("@prisma/client");
+const prisma = new PrismaClient();
+const fs = require("fs");
+
+async function main() {
+  const data = JSON.parse(
+    fs.readFileSync("prisma/seeders/data/mobilOneCenterLocation.json", "utf-8"),
+  );
+
+  for (const location of data.Locations) {
+    const [openingHours, closingHours] =
+      location.HoursOfOperation24[0].hours.split(" - ");
+
+    const [openingHour, openingMinute] = openingHours.split(":").map(Number);
+    const [closingHour, closingMinute] = closingHours.split(":").map(Number);
+
+    const openingTime = openingHour * 60 + openingMinute;
+    const closingTime = closingHour * 60 + closingMinute;
+
+    await prisma.mobilCenter.create({
+      data: {
+        lineUid: null,
+        name: location.DisplayName,
+        address: `${location.AddressLine1}, ${location.AddressLine2}, ${location.City}, ${location.StateProvince}, ${location.PostalCode}`,
+        latitude: location.Latitude,
+        longitude: location.Longitude,
+        openingTime: openingTime,
+        closingTime: closingTime,
+      },
+    });
+  }
+
+  console.log("Seeding MobilCenters completed!");
+  process.exit(0);
+}
+
+main()
+  .catch((e) => {
+    console.error(e);
+    process.exit(1);
+  })
+  .finally(async () => {
+    await prisma.$disconnect();
+  });
