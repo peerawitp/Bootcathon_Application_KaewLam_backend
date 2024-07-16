1d0d7118bc6170100697eed346f92b6b3805e8f6
Peerawit Pharkdeepinyo <peerawitp@protonmail.com>
2024-07-16 14:40:36 +0700

    feat: add specific center route

--- src/controllers/center.ts
+++ src/controllers/center.ts
@@ -5,10 +5,21 @@ import { db } from "../db";
 
 export const center = async (app: Elysia) =>
   app.group("/center", (app) =>
-    app.use(isLiffAuthenticated).get("/", async () => {
-      const centers = await db.mobilCenter.findMany();
-      return centers;
-    }),
+    app
+      .use(isLiffAuthenticated)
+      .get("/", async () => {
+        const centers = await db.mobilCenter.findMany();
+        return centers;
+      })
+      .get("/:id", async ({ params }) => {
+        const center = await db.mobilCenter.findUnique({
+          where: { id: parseInt(params.id) },
+        });
+        if (!center) {
+          throw new Error("Center not found");
+        }
+        return center;
+      }),
   );
 
 export default center;
