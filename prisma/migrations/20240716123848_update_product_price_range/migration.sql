fac066fa6f88f704cb0a4b216d81736e9eb3dc7c
Peerawit Pharkdeepinyo <peerawitp@protonmail.com>
2024-07-16 19:40:52 +0700

    feat: update product priceRange schema

---
+++ prisma/migrations/20240716123848_update_product_price_range/migration.sql
@@ -0,0 +1,9 @@
+/*
+  Warnings:
+
+  - You are about to drop the column `price` on the `Product` table. All the data in the column will be lost.
+
+*/
+-- AlterTable
+ALTER TABLE "Product" DROP COLUMN "price",
+ADD COLUMN     "priceRange" DOUBLE PRECISION[];
